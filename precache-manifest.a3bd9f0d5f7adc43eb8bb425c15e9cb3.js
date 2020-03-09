1fddda4f5f0c213ab4c7774db8a18908ae5849c6
Ruben Giannotti <ruben.giannotti@gmx.net>
2020-03-09 13:25:53 +0100

    Commit correct files

---
+++ precache-manifest.a3bd9f0d5f7adc43eb8bb425c15e9cb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a56e502c55b43a784f962ae290a5006",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0ddd66b8769c26f3c32",
+    "url": "/static/css/main.932cbb7d.chunk.css"
+  },
+  {
+    "revision": "aa4c7d9e68f9059800c8",
+    "url": "/static/js/2.4040cf8a.chunk.js"
+  },
+  {
+    "revision": "7bfcea31855bd57860628e5726359b29",
+    "url": "/static/js/2.4040cf8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ddd66b8769c26f3c32",
+    "url": "/static/js/main.bddf0542.chunk.js"
+  },
+  {
+    "revision": "306ffb06ab334ecb4f3b",
+    "url": "/static/js/runtime-main.93d33518.js"
+  }
+]);
