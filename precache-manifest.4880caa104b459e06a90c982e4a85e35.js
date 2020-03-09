013ce16620ad3fdb2ae43556d23772d39c16a717
Ruben Giannotti <ruben.giannotti@gmx.net>
2020-03-09 13:04:48 +0100

    Initial commit

---
+++ precache-manifest.4880caa104b459e06a90c982e4a85e35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ccd8397108ec3003155858fe586537",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "35f7f8e2b45b1a6353ed",
+    "url": "/react-gh-pages/static/css/main.932cbb7d.chunk.css"
+  },
+  {
+    "revision": "af8160ca0c24778a520b",
+    "url": "/react-gh-pages/static/js/2.9119bbfb.chunk.js"
+  },
+  {
+    "revision": "7bfcea31855bd57860628e5726359b29",
+    "url": "/react-gh-pages/static/js/2.9119bbfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f7f8e2b45b1a6353ed",
+    "url": "/react-gh-pages/static/js/main.a76e26ab.chunk.js"
+  },
+  {
+    "revision": "7f8740b6bceeb51ed1ea",
+    "url": "/react-gh-pages/static/js/runtime-main.78c91af0.js"
+  }
+]);
